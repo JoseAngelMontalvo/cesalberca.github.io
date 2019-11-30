77dbedecfc66c770be0053e8f8148de42c35d51f
César Alberca <cesalberca@gmail.com>
2019-11-30 12:12:50 +0100

    Updates

---
+++ precache-manifest.66f37ef0f9b50671959d024ab76c8849.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e01f31d285350f4c3cc8b77cdd15a0c1",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "e074f6fb19066c94fd86d936ea602d0c",
+    "url": "/assets/abstract-dark-design.jpg"
+  },
+  {
+    "revision": "f768d7886dfb04abf82763736eb695e1",
+    "url": "/assets/art-artistic-canvas.jpg"
+  },
+  {
+    "revision": "0f306a67caae1379aadc10cb095a9d72",
+    "url": "/assets/fog.gif"
+  },
+  {
+    "revision": "3ff4463ed1cb22e65278c438bd2ebd87",
+    "url": "/assets/migrating-to-typescript/image-20191112213355655.png"
+  },
+  {
+    "revision": "f84cab10e99d1f1957c0b7231016ba80",
+    "url": "/assets/migrating-to-typescript/image-20191112213602568.png"
+  },
+  {
+    "revision": "486f88651516f5dc5288fe42aac6b02c",
+    "url": "/assets/migrating-to-typescript/image-20191112214326825.png"
+  },
+  {
+    "revision": "776df250c5e9b79ece28f5064553bdaf",
+    "url": "/assets/migrating-to-typescript/image-20191112214957394.png"
+  },
+  {
+    "revision": "b2343a33ce42691bb884e9fba61f91d8",
+    "url": "/assets/migrating-to-typescript/image-20191113201553918.png"
+  },
+  {
+    "revision": "1bd32f5b1f7c77bb315ba00a592be38d",
+    "url": "/assets/migrating-to-typescript/image-20191113202705616.png"
+  },
+  {
+    "revision": "033fb196da3a2905348d8d7fc3f85a8f",
+    "url": "/assets/migrating-to-typescript/image-20191113203351897.png"
+  },
+  {
+    "revision": "a6aad4660e4b1db88f045b54969ab399",
+    "url": "/assets/migrating-to-typescript/image-20191115194956149.png"
+  },
+  {
+    "revision": "42a61ebcc1a7b46259ab179338480047",
+    "url": "/assets/rainbow.jpeg"
+  },
+  {
+    "revision": "dad3fac57cd813099f35",
+    "url": "/css/about~article.ab80dd5b.css"
+  },
+  {
+    "revision": "eceb183efd220cc36999",
+    "url": "/css/app.f15befcc.css"
+  },
+  {
+    "revision": "f6c2063e45092b356555",
+    "url": "/css/article.eb04806c.css"
+  },
+  {
+    "revision": "881bc9ed8de1f966866b",
+    "url": "/css/articles.ce4c32a6.css"
+  },
+  {
+    "revision": "05bc513c08c57645c8d0",
+    "url": "/css/home.37a506d1.css"
+  },
+  {
+    "revision": "fb2cb4a66d00cdf12e25",
+    "url": "/css/talks.fdc9bb9f.css"
+  },
+  {
+    "revision": "e074f6fb19066c94fd86d936ea602d0c",
+    "url": "/img/abstract-dark-design.e074f6fb.jpg"
+  },
+  {
+    "revision": "f768d7886dfb04abf82763736eb695e1",
+    "url": "/img/art-artistic-canvas.f768d788.jpg"
+  },
+  {
+    "revision": "0f306a67caae1379aadc10cb095a9d72",
+    "url": "/img/fog.0f306a67.gif"
+  },
+  {
+    "revision": "3ff4463ed1cb22e65278c438bd2ebd87",
+    "url": "/img/image-20191112213355655.3ff4463e.png"
+  },
+  {
+    "revision": "f84cab10e99d1f1957c0b7231016ba80",
+    "url": "/img/image-20191112213602568.f84cab10.png"
+  },
+  {
+    "revision": "486f88651516f5dc5288fe42aac6b02c",
+    "url": "/img/image-20191112214326825.486f8865.png"
+  },
+  {
+    "revision": "776df250c5e9b79ece28f5064553bdaf",
+    "url": "/img/image-20191112214957394.776df250.png"
+  },
+  {
+    "revision": "b2343a33ce42691bb884e9fba61f91d8",
+    "url": "/img/image-20191113201553918.b2343a33.png"
+  },
+  {
+    "revision": "1bd32f5b1f7c77bb315ba00a592be38d",
+    "url": "/img/image-20191113202705616.1bd32f5b.png"
+  },
+  {
+    "revision": "033fb196da3a2905348d8d7fc3f85a8f",
+    "url": "/img/image-20191113203351897.033fb196.png"
+  },
+  {
+    "revision": "a6aad4660e4b1db88f045b54969ab399",
+    "url": "/img/image-20191115194956149.a6aad466.png"
+  },
+  {
+    "revision": "1420632c4110c8229fe200a62c8a0dcf",
+    "url": "/img/me.1420632c.png"
+  },
+  {
+    "revision": "42a61ebcc1a7b46259ab179338480047",
+    "url": "/img/rainbow.42a61ebc.jpeg"
+  },
+  {
+    "revision": "3594ccac3498f7dc8f0e2883c7b7812d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e0e79c7baf8b66bccba",
+    "url": "/js/about.eaf61dee.js"
+  },
+  {
+    "revision": "dad3fac57cd813099f35",
+    "url": "/js/about~article.0bea81df.js"
+  },
+  {
+    "revision": "eceb183efd220cc36999",
+    "url": "/js/app.48f8730a.js"
+  },
+  {
+    "revision": "f6c2063e45092b356555",
+    "url": "/js/article.987c4dee.js"
+  },
+  {
+    "revision": "881bc9ed8de1f966866b",
+    "url": "/js/articles.5c2ef6ff.js"
+  },
+  {
+    "revision": "75b887978a12f0a7aa84",
+    "url": "/js/chunk-vendors.5d1fc025.js"
+  },
+  {
+    "revision": "05bc513c08c57645c8d0",
+    "url": "/js/home.f7678e6b.js"
+  },
+  {
+    "revision": "fb2cb4a66d00cdf12e25",
+    "url": "/js/talks.8502969f.js"
+  },
+  {
+    "revision": "9cf4336f3af850d9c1df42e24cfbfca8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.784e907418f5922677127d8b153f8935.js"
+  "/precache-manifest.66f37ef0f9b50671959d024ab76c8849.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "blog"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "blog"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
